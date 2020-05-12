acf73b2c3a9dc348aa585493227857d9578abf5a
Danir1406 <danir2025@mail.ru>
2020-05-12 12:00:19 +0500

    first commit

--- main.py
+++ main.py
@@ -428,7 +428,7 @@ def send_password_reset_email(user):
     </a>.
 </p>
 <p>If you have not requested a password reset simply ignore this message.</p>
-<p>Sincerely,</p>
+<p>Sincerely</p>
 <p>WebSite TeaM</p>"""
     msg.attach(MIMEText(html, 'html'))
     server = smtplib.SMTP('smtp.mail.ru', 25)
